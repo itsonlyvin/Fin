0ab0d407f88b86abced1ef637c361906bc3ce982
vinayak <vinayakvichuvickey778@gmail.com>
2025-09-12 17:13:39 +0530

    onboarding updated

--- lib/features/authentication/screens/onboarding/onboardging.dart
+++ lib/features/authentication/screens/onboarding/onboardging.dart
@@ -1,71 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:t_store/features/app/screens/employee/attendance_history/widgets/specialColumn.dart';
 import 'package:t_store/features/authentication/screens/login/login.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
-import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class Onboardging extends StatelessWidget {
   const Onboardging({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(TSizes.spaceBtwSections),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => Get.to(
-                    () => const LoginScreen(
-                        logo: TImages.finDarkAppLogo,
-                        color1: TColors.fin1,
-                        color2: TColors.fin2),
-                  ),
-                  child: const Text(TTexts.companyName),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.spaceBtwSections),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ////
+            GestureDetector(
+              onTap: () => Get.to(
+                () => const LoginScreen(
+                    logo: TImages.finDarkAppLogo,
+                    color1: TColors.fin1,
+                    color2: TColors.fin2),
+              ),
+              child: SpecialColumn(
+                dark: dark,
+                child: Image(
+                  height: 150,
+                  image: AssetImage(
+                      dark ? TImages.finDarkAppLogo : TImages.finLightAppLogo),
                 ),
               ),
-              const SizedBox(
-                height: TSizes.spaceBtwItems,
+            ),
+
+/////
+            GestureDetector(
+              onTap: () => Get.to(
+                () => const LoginScreen(
+                    logo: TImages.openDarkAppLogo,
+                    color1: Color.fromARGB(255, 203, 140, 140),
+                    color2: Color.fromARGB(255, 206, 103, 197)),
               ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => Get.to(
-                    () => const LoginScreen(
-                        logo: TImages.openDarkAppLogo,
-                        color1: Color.fromARGB(255, 203, 140, 140),
-                        color2: Color.fromARGB(255, 206, 103, 197)),
-                  ),
-                  child: const Text(TTexts.companyName1),
+              child: SpecialColumn(
+                dark: dark,
+                child: Image(
+                  height: 150,
+                  image: AssetImage(dark
+                      ? TImages.openDarkAppLogo
+                      : TImages.openLightAppLogo),
                 ),
               ),
-              const SizedBox(
-                height: TSizes.spaceBtwItems,
+            ),
+
+            GestureDetector(
+              onTap: () => Get.to(
+                () => const LoginScreen(
+                    admin: true,
+                    logo: TImages.finDarkAppLogo,
+                    color1: TColors.fin1,
+                    color2: TColors.fin2),
               ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => Get.to(
-                    () => const LoginScreen(
-                        admin: true,
-                        logo: TImages.finDarkAppLogo,
-                        color1: TColors.fin1,
-                        color2: TColors.fin2),
-                  ),
-                  child: const Text("Admin"),
-                ),
-              ),
-              const SizedBox(
-                height: TSizes.spaceBtwItems,
-              ),
-            ],
-          ),
+              child: SpecialColumn(
+                  dark: dark,
+                  child: SizedBox(
+                    height: 150,
+                    child: Center(
+                      child: Text(
+                        "ADMIN",
+                        style: Theme.of(context).textTheme.displayLarge,
+                      ),
+                    ),
+                  )),
+            ),
+          ],
         ),
       ),
     );
